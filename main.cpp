6a255ef736e71ffb10dc6a4e11257ce71d85e518
Adel <adelkhakim16@gmail.com>
2026-09-24 13:40:38 +0300

    Initial commin: Hello World на C++ и Python

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Студент: Хакимов Адель" << std::endl;
+    std::cout << "Дата: 2026-09-24";
+    return 0;
+}
