d84ae6ed2dab4a8f7f117d853012be9b7349b8ad
Adel <adelkhakim16@gmail.com>
2026-09-24 14:07:40 +0300

    Initial commit: Hello World на C++ и Pthon V2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <windows.h>
 
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
     std::cout << "Hello World!" << std::endl;
     std::cout << "Студент: Хакимов Адель" << std::endl;
     std::cout << "Дата: 2026-09-24";
